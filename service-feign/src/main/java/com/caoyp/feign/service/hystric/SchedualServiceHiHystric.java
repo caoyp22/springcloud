d36714384ecd050b52ccbac095005cb31716f939
caoypa <caoypa@digitalchina.com>
2018-06-12 20:36:42 +0800

    初始化项目

---
+++ service-feign/src/main/java/com/caoyp/feign/service/hystric/SchedualServiceHiHystric.java
@@ -0,0 +1,12 @@
+package com.caoyp.feign.service.hystric;
+
+import com.caoyp.feign.service.SchedualServiceHi;
+import org.springframework.stereotype.Component;
+
+@Component
+public class SchedualServiceHiHystric implements SchedualServiceHi {
+    @Override
+    public String sayHiFromClientOne(String name) {
+        return "sorry "+name;
+    }
+}
